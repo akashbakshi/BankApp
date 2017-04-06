127912410943b5b45327fa2f1cec96d547ee73de
Akash Bakshi <akashbakshi@Akashs-MacBook-Pro.local>
2017-04-05 22:56:06 -0400

    Initial commit

---
+++ app/src/main/java/com/example/akashbakshi/bankapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.akashbakshi.bankapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signUp = (Button)findViewById(R.id.btnSignUp);
+
+        signUp.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent signIntent = new Intent(MainActivity.this,SignUp.class);
+                        startActivity(signIntent);
+                    }
+                }
+        );
+    }
+}
---
+++ app/src/main/java/com/example/akashbakshi/bankapp/SignUp.java
@@ -0,0 +1,19 @@
+package com.example.akashbakshi.bankapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by akashbakshi on 2017-04-05.
+ */
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+    }
+}
